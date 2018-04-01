631b4bc18c2b5f37771d0c52ca19f373cca4d86e
Adam Fields <adam.elliot.fields@gmail.com>
2018-04-01 11:58:58 -0400

    Refactored.

--- cpu/index.js
+++ cpu/index.js
@@ -1,56 +1,10 @@
-const micro = require('micro');
-const morgan = require('morgan');
-const { EOL, cpus, hostname } = require('os');
-const { cpuUsage, cpuFree } = require('os-utils');
-const { address } = require('ip');
+const server = require('./server');
+const { doShutdown, getProduction } = require('./utils');
 
-const isProduction = process.env.NODE_ENV === 'production';
+const production = getProduction();
 
-const PORT = process.env.PORT || (isProduction ? 80 : 8080);
-const HOST = isProduction ? '0.0.0.0' : 'localhost';
-const LOG_FORMAT = isProduction ? 'combined' : 'dev';
-
-// Make cpuUsage and cpuFree return a Promise so they can be awaited
-const getCpuUsage = () => (
-  new Promise(resolve => {
-    cpuUsage(usage => resolve(`${(usage * 100).toFixed(0)}%`));
-  })
-);
-
-const getCpuFree = () => (
-  new Promise(resolve => {
-    cpuFree(free => resolve(`${(free * 100).toFixed(0)}%`));
-  })
-);
-
-// Quit the process after all connections have closed
-const shutdown = () => {
-  console.warn(`${EOL}Server shutting down...`);
-  process.exit(0);
-};
-
-const server = micro((request, response) => {
-  morgan(LOG_FORMAT)(request, response, async error => {
-    if (error) console.error(error);
-
-    const model = cpus()[0]['model'];
-    const cores = cpus().length;
-
-    const usage = await getCpuUsage();
-    const free = await getCpuFree();
-
-    micro.send(response, 200, {
-      model,
-      cores,
-      usage,
-      free,
-      meta: {
-        hostname: hostname(),
-        ip: address()
-      }
-    });
-  });
-});
+const PORT = process.env.PORT || (production ? 80 : 8080);
+const HOST = production ? '0.0.0.0' : 'localhost';
 
 server.listen(PORT, HOST, () => {
   console.info(`Server listening at http://${HOST}:${PORT} ...`);
@@ -58,10 +12,10 @@ server.listen(PORT, HOST, () => {
 
 // Handle Ctrl-C
 process.on('SIGINT', () => {
-  server.close(shutdown);
+  server.close(doShutdown);
 });
 
 // Docker sends SIGTERM followed by SIGKILL after 10 seconds
 process.on('SIGTERM', () => {
-  server.close(shutdown);
+  server.close(doShutdown);
 });
